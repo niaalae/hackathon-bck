e42908ae80aed4e76a4504ec20ebba15a4e71867
WHISKY <mohammedmeski200@gmail.com>
2026-03-20 09:29:49 +0000

    refactor: remove unused environment variable import

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,3 @@
-import '../env'
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
